ceff6c0725f61fcceba9cf2254b434a7f2c67e16
Michael Robertson <mrobertson00@stuy.edu>
2019-11-14 00:07:20 -0500

    e

--- infostat.c
+++ infostat.c
@@ -22,11 +22,17 @@ int main() {
     printf("mode: %o\n",info.st_mode);
     printf("time of last access: %s\n",ctime(&info.st_atime));
 
-    printf("\nfile size:\n");
+    printf("file size:\n");
     printf("%ld B\n",info.st_size);
     printf("%ld KB\n",info.st_size / 1000);
     printf("%ld MB\n",info.st_size / 1000000);
     printf("%ld GB\n",info.st_size / 1000000000);
+
+    printf("testing understanding of bitwise and: %o\nexpected: 4", 5 & 7);
+
+    printf("ls -l format:\n");
+
+    int size = 511 & info.st_size;
 
     return 0;
 }
