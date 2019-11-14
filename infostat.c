797ccf6d45a9cde8048e53373c77768d2fbadf2e
Michael Robertson <mrobertson00@stuy.edu>
2019-11-13 23:25:09 -0500

    e

---
+++ infostat.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+
+    struct stat info;
+
+    int i = stat("file.txt",&info);
+
+    if (i == -1) {
+        printf("stat failed\n");
+        printf("%s\n",strerro(errno));
+    }
+
+    printf("file size: %ld\n",info.st_size);
+    printf("mode: %o\n",info.st_mode);
+    printf("time of last access: %s",ctime(info.st_atime));
+
+    return 0;
+}
